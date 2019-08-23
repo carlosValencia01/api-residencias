65c1c0d95351e78940e8064f7c25ae5784746105
Agustin Barajas <agbarajasva@ittepic.edu.mx>
2019-08-23 14:02:45 -0600

    Uncomment the separate emails function

--- controllers/inscription.controller.js
+++ controllers/inscription.controller.js
@@ -1,63 +1,76 @@
 const status = require('http-status');
 const fs = require('fs');
 const path = require('path');
-const util= require('util');
-const sendMail= require('./mail.controller');
+const util = require('util');
+const sendMail = require('./mail.controller');
 
 const readFile = util.promisify(fs.readFile);
 
 let _inscription;
 let _period;
 
-const sendTemplateMail= async (req, res)=>{
-    const template= chooseTemplate(req.body.index);
+const sendTemplateMail = async (req, res) => {
+    const template = chooseTemplate(req.body.index);
 
     const filePath = path.resolve('./templates/', template.file);
 
-    readFile(filePath, {encoding: 'utf-8'}).then(data=>{
-        req.body.sender= template.sender;
-        req.body.subject= template.subject;
-        req.body.message= data;
+    readFile(filePath, {
+        encoding: 'utf-8'
+    }).then(data => {
+        req.body.sender = template.sender;
+        req.body.subject = template.subject;
+        req.body.message = data;
 
-        if(req.body.many){
-            getAllMailsFromDb().then(allEmails=>{
+        if (req.body.many) {
+            getAllMailsFromDb().then(allEmails => {
                 asyncFor(req, res, allEmails);
-            }).catch(err=>{
-                res.json({ code: 400, message: 'Ocurrió un error al buscar los emails en la db', status: status.BAD_REQUEST, detail: err});
+            }).catch(err => {
+                res.json({
+                    code: 400,
+                    message: 'Ocurrió un error al buscar los emails en la db',
+                    status: status.BAD_REQUEST,
+                    detail: err
+                });
             });
-        }else if(req.body.to_email.length>1){
-            let flag= false;
-            //const allEmails= separateEmails(req.body.to_email);
+        } else if (req.body.to_email.length > 1) {
+            let flag = false;
+            const allEmails = separateEmails(req.body.to_email);
 
-            if(req.body.index===1){
-                flag= true;
+            if (req.body.index === 1) {
+                flag = true;
             }
-            asyncFor(req, res, req.body.to_email, flag);
-        }else{
-            sendMail(req).then(async data=>{
-                if(req.body.index===1){
+            asyncFor(req, res, allEmails, flag);
+        } else {
+            sendMail(req).then(async data => {
+                if (req.body.index === 1) {
                     await saveEmails(req.body.to_email);
                 }
+                data.code = 200;
                 res.json(data);
-            }).catch(err=>{
+            }).catch(err => {
                 res.json(err);
             });
         }
-    }).catch(err=>{
-        res.json({ code: 400, message: 'Error al buscar el template', status: status.BAD_REQUEST, detail: err });
+    }).catch(err => {
+        res.json({
+            code: 400,
+            message: 'Error al buscar el template',
+            status: status.BAD_REQUEST,
+            detail: err
+        });
     });
 };
 
-const asyncFor= async (req, res, allEmails, save=false)=>{
-    let messages= [];
-    let allFailedEmails= [];
+const asyncFor = async (req, res, allEmails, save = false) => {
+    let messages = [];
+    let allFailedEmails = [];
 
-    for(let i=0;i<allEmails.length;i++){
-        req.body.to_email= allEmails[i];
+    for (let i = 0; i < allEmails.length; i++) {
+        req.body.to_email = allEmails[i];
 
-        await sendMail(req, res).then(async data=>{
-            if(save){
-                if(data.code===202){
+        await sendMail(req, res).then(async data => {
+            if (save) {
+                if (data.code === 202) {
                     await saveEmails(allEmails[i]);
                 }
             }
@@ -65,19 +78,27 @@ const asyncFor= async (req, res, allEmails, save=false)=>{
 
             messages.push(data);
 
-            if(data.code!==202){
-                allFailedEmails= allFailedEmails.concat(allEmails[i]);
+            if (data.code !== 202) {
+                allFailedEmails = allFailedEmails.concat(allEmails[i]);
             }
 
-            if(i===(allEmails.length-1)){
-                for(const message of messages){
-                    if(message.code!==202){
-                        return res.json({code:message.code, emails:allFailedEmails});
+            if (i === (allEmails.length - 1)) {
+                for (const message of messages) {
+                    if (message.code !== 202) {
+                        return res.json({
+                            code: message.code,
+                            emails: allFailedEmails
+                        });
                     }
                 }
-                return res.json({ code: 200, message: "Emails enviados correctamente", status:status.OK, detail: 'OK' });
+                return res.json({
+                    code: 200,
+                    message: "Emails enviados correctamente",
+                    status: status.OK,
+                    detail: 'OK'
+                });
             }
-        }).catch(err=>{
+        }).catch(err => {
             console.log(err);
         });
     }
@@ -93,43 +114,49 @@ const asyncFor= async (req, res, allEmails, save=false)=>{
     });
 };*/
 
-const saveEmails= async emails=>{
+const saveEmails = async emails => {
     let newAllEmails = [];
 
-    await _period.find({active:true}, {_id:0, id:1, year:1}).then(doc=>{
-        for(const email of emails){
-            const newEmails={
-                email:email,
-                period: doc[0]._doc.year+""+doc[0]._doc.id,
+    await _period.find({
+        active: true
+    }, {
+        _id: 0,
+        id: 1,
+        year: 1
+    }).then(doc => {
+        for (const email of emails) {
+            const newEmails = {
+                email: email,
+                period: doc[0]._doc.year + "" + doc[0]._doc.id,
                 //sent:true
             };
             newAllEmails.push(newEmails);
         }
-    }).catch(err=>{
+    }).catch(err => {
         console.log(err);
     });
 
-    return new Promise((resolve, reject)=>{
-        _inscription.create(newAllEmails).then(_=>{
+    return new Promise((resolve, reject) => {
+        _inscription.create(newAllEmails).then(_ => {
             resolve(true);
-        }).catch(_=>{
+        }).catch(_ => {
             reject(false);
         });
     });
 };
 
-const getAllMailsFromDb= _=>{
-    return new Promise((resolve, reject)=>{
-        _inscription.distinct("email").then(data=>{
-            const allEmails= separateEmails(data);
+const getAllMailsFromDb = _ => {
+    return new Promise((resolve, reject) => {
+        _inscription.distinct("email").then(data => {
+            const allEmails = separateEmails(data);
             resolve(allEmails);
-        }).catch(err=>{
+        }).catch(err => {
             reject(err);
         });
     });
 };
 
-const separateEmails= data=>{
+const separateEmails = data => {
     const length = data.length;
     const iterations = Math.floor((length - 1) / 1000);
     const res = (length % 1000);
@@ -150,32 +177,32 @@ const separateEmails= data=>{
     }
 };
 
-const chooseTemplate= index=>{
-    switch(index){
+const chooseTemplate = index => {
+    switch (index) {
         case 1:
             return {
-                file:"inscription.html",
-                sender:"Servicios escolares <escolares_05@ittepic.edu.mx>",
-                subject:"Proceso de inscripción"
+                file: "inscription.html",
+                    sender: "Servicios escolares <escolares_05@ittepic.edu.mx>",
+                    subject: "Proceso de inscripción"
             };
         case 2:
             return {
-                file:"english.html",
-                sender:"Centro de idiomas <coordinacion.le@ittepic.edu.mx>",
-                subject:"Promoción de cursos de inglés"
+                file: "english.html",
+                    sender: "Centro de idiomas <coordinacion.le@ittepic.edu.mx>",
+                    subject: "Promoción de cursos de inglés"
             };
         case 3:
             return {
-                file:"credentials.html",
-                sender:"Servicios escolares <escolares_05@ittepic.edu.mx>",
-                subject:"Tómate la foto para tu credencial"
+                file: "credentials.html",
+                    sender: "Servicios escolares <escolares_05@ittepic.edu.mx>",
+                    subject: "Tómate la foto para tu credencial"
             };
     }
 };
 
-module.exports = (Inscription, Period)=>{
-    _inscription= Inscription;
-    _period= Period;
+module.exports = (Inscription, Period) => {
+    _inscription = Inscription;
+    _period = Period;
     return ({
         sendTemplateMail
     });
