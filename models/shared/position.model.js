e91c9cedf3f6b62841416b3dd4b0cdbac48bfbd1
Agustin Barajas <agbarajasva@ittepic.edu.mx>
2019-10-31 17:20:54 -0700

    feat: Positions management
    Add field (signature or non-signature) in positions

--- models/shared/position.model.js
+++ models/shared/position.model.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 let positionSchema = mongoose.Schema({
-    name: { type: String },
-    ascription: { type: mongoose.Schema.Types.ObjectId, ref: 'Department'},
-    documents: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Document' }]
+    name: {type: String},
+    ascription: {type: mongoose.Schema.Types.ObjectId, ref: 'Department'},
+    documents: [{type: mongoose.Schema.Types.ObjectId, ref: 'Document'}],
+    canSign: {type: Boolean, default: false},
 });
 
 const positionModel = mongoose.model('Position', positionSchema, 'positions');
