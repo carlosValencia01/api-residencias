dbfa8f4c0831c77eafe46fcdce9adb2c674d2477
rijimenezes <rijimenezes@ittepic.edu.mx>
2019-11-04 22:05:47 -0700

    Feat: New period model

--- models/app/period.model.js
+++ models/app/period.model.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
 
 const periodSchema = new mongoose.Schema({    
-    name: String,
+    periodName: String,
     year: String,
     active: Boolean,   
     initDate :{ type: Date, default: new Date},
-    endDate :{ type: Date, required: false},    
-    inscriptionStudentsNumber : {type: Number, required: false}
+    endDate :{ type: Date},    
+    insPerInitDate:{type: Date},
+    insPerEndDate:{type: Date},
+    arecPerInitDate:{type: Date},
+    arecPerEndDate:{type: Date},
+    arecInitShed:{type: Number},
+    arecEndShed:{type: Number},
 });
 
 const periodModel = mongoose.model('Period', periodSchema, 'periods');
