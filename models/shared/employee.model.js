e050cbd8165fc22f3042e214ce48a14d1de84ad6
Agustin Barajas <agbarajasva@ittepic.edu.mx>
2019-11-25 21:55:47 -0700

    fix: Add CURP field to employee

--- models/shared/employee.model.js
+++ models/shared/employee.model.js
@@ -1,22 +1,23 @@
 const mongoose = require('mongoose');
 
 let employeeSchema = new mongoose.Schema({
-    rfc: { type: String, unique: true, uppercase: true, trim: true, minlength: 10, maxlength: 13 },
+    rfc: {type: String, unique: true, uppercase: true, trim: true, minlength: 10, maxlength: 13},
+    curp: {type: String, unique: true, uppercase: true, trim: true, minlength: 18, maxlength: 18},
     name: {
-        firstName: { type: String, required: true, uppercase: true, trim: true },
-        lastName: { type: String, required: true, uppercase: true, trim: true },
-        fullName: { type: String, required: true, uppercase: true, trim: true }
+        firstName: {type: String, required: true, uppercase: true, trim: true},
+        lastName: {type: String, required: true, uppercase: true, trim: true},
+        fullName: {type: String, required: true, uppercase: true, trim: true}
     },
-    gender: { type: String, trim: true, enum: ['FEMENINO', 'MASCULINO', 'OTRO'] },
-    birthDate: { type: Date },
-    filename: { type: String },
+    gender: {type: String, trim: true, enum: ['FEMENINO', 'MASCULINO', 'OTRO']},
+    birthDate: {type: Date},
+    filename: {type: String},
     grade: [{
         _id: false,
-        title: { type: String, uppercase: true, trim: true },
-        cedula: { type: String, uppercase: true, trim: true },
-        abbreviation: { type: String, uppercase: true, trim: true },
-        level: { type: String, enum: ['DOCTORADO', 'MAESTRIA', 'LICENCIATURA'], uppercase: true, trim: true },
-        default: { type: Boolean, default: false }
+        title: {type: String, uppercase: true, trim: true},
+        cedula: {type: String, uppercase: true, trim: true},
+        abbreviation: {type: String, uppercase: true, trim: true},
+        level: {type: String, enum: ['DOCTORADO', 'MAESTRIA', 'LICENCIATURA'], uppercase: true, trim: true},
+        default: {type: Boolean, default: false}
     }],
     positions: [{
         _id: false,
