519103155cb60b072ce370911907b1d771b1751d
Agustin Barajas <agbarajasva@ittepic.edu.mx>
2019-12-09 15:09:37 -0700

    fix: See teachers for jury assignment

--- routers/app/department.router.js
+++ routers/app/department.router.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
 module.exports = (wagner) => {
-    const deptoCtrl = wagner.invoke((Department) =>
-        require('../../controllers/app/department.controller')(Department));
+    const deptoCtrl = wagner.invoke((Department, Employee) =>
+        require('../../controllers/app/department.controller')(Department, Employee));
         
         router.get('/employees', (req, res) => deptoCtrl.getAll(req, res));                
 
