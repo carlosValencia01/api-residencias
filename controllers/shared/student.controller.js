1d04557f59d70e0788b1ce80dee84b7b3af2d7c3
rijimenezes <rijimenezes@ittepic.edu.mx>
2020-01-13 17:03:51 -0700

    Fix: Documents without status

--- controllers/shared/student.controller.js
+++ controllers/shared/student.controller.js
@@ -645,16 +645,34 @@ const getStudentRoleId = () => {
 const getDocumentsStatus = async (req,res)=>{
     const { _id } = req.params;
     let id = mongoose.Types.ObjectId(_id);       
-    const documents = await queryDocuments(id);    
+    const documents = await queryDocuments(id);        
     
-    if(documents.error){
+    if(documents == null){
+        res.status(status.BAD_REQUEST).json({
+            error: 'No tiene documentos',        
+            action: 'get documents status'
+        });
+    }else if(documents.error){
         res.status(status.BAD_REQUEST).json({
             error: documents.error,        
             action: 'get documents status'
         });
     }else{
         res.status(status.OK).json({
-            documents:documents.map( doc=> ({filename:doc.filename,statusName:doc.status.filter(st=> st.active == true)[0].name})).filter(docFiltered=>docFiltered.filename.indexOf('SOLICITUD') < 0 && docFiltered.filename.indexOf('CONTRATO') < 0),
+            documents:documents.map( 
+                doc=> 
+                    {
+                        const stat = doc.status.filter(
+                            st=> st.active == true)[0];
+                        return {
+                        filename:doc.filename,
+                        statusName: stat ? stat.name : null}
+                    }
+                
+                
+                ).filter(
+                    docFiltered=>docFiltered.filename.indexOf('SOLICITUD') < 0 && docFiltered.filename.indexOf('CONTRATO') < 0 && docFiltered.statusName !== null
+                    ),
             action: 'get documents status'
         });
     }
