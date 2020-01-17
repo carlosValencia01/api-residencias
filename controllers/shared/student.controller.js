4be64b26271225aabc95b82a052883fa9ba190ab
rijimenezes <rijimenezes@ittepic.edu.mx>
2020-01-16 17:30:00 -0700

    Feat: Expedient with documents modfied

--- controllers/shared/student.controller.js
+++ controllers/shared/student.controller.js
@@ -62,6 +62,7 @@ const getStudentsInscription = (req, res) => {
                 "dateAcceptedTerms": student.dateAcceptedTerms,
                 "printCredential": student.printCredential,
                 "warningAnalysis": student.warningAnalysis, 
+                documentsModified : documentsHaveChanges(student.documents,student.inscriptionStatus),
                 totalDocumentsNumber: mapDocuments(student.documents).length,
                     documentsReviewNumber: mapDocuments(student.documents).filter( doc=>doc.statusName !== 'EN PROCESO').length,
                     documentsLastStatus: mapDocuments(student.documents)        
@@ -86,6 +87,25 @@ const mapDocuments = (documents)=>{
         ).filter(
             docFiltered=>docFiltered.filename.indexOf('SOLICITUD') < 0 && docFiltered.filename.indexOf('CONTRATO') < 0 && docFiltered.statusName !== null
             );
+};
+const documentsHaveChanges = (documents, status)=>{
+    if(status == 'En Proceso'){
+        
+        const changes = documents.filter( doc => doc.filename.indexOf('SOLICITUD') < 0 && doc.filename.indexOf('CONTRATO') < 0).map(
+            filteredDoc=>{
+                if(filteredDoc.status.length > 1){
+                    const prevStatus = filteredDoc.status[filteredDoc.status.length-2];
+                    const curStatus = filteredDoc.status[filteredDoc.status.length-1];
+                
+                    return prevStatus.name == 'RECHAZADO' ? curStatus.name == 'EN PROCESO' && curStatus.message == 'Se actualizo el documento' ? {filename:filteredDoc.filename, moified:true} : {filename:filteredDoc.filename, moified:false} :{filename:filteredDoc.filename, moified:false};
+                }else {return {err:false};}
+            }
+        ).filter(mapedDocument=> mapedDocument.moified == true);          
+        return changes.length > 0 ? 'true' : 'false';
+    }else{
+        return 'false';
+    }
+            
 };
 
 const getStudentsInscriptionLogged = (req, res) => {
@@ -137,7 +157,9 @@ const getStudentsInscriptionProcess = (req, res) => {
                 "acceptedTerms": student.acceptedTerms,
                 "dateAcceptedTerms": student.dateAcceptedTerms,
                 "printCredential": student.printCredential,
-                "warningAnalysis": student.warningAnalysis, 
+                "warningAnalysis": student.warningAnalysis
+                , 
+                documentsModified : documentsHaveChanges(student.documents,student.inscriptionStatus), 
                 totalDocumentsNumber: mapDocuments(student.documents).length,
                     documentsReviewNumber: mapDocuments(student.documents).filter( doc=>doc.statusName !== 'EN PROCESO').length,
                     documentsLastStatus: mapDocuments(student.documents)        
@@ -904,6 +926,20 @@ const getDocumentsStatus = async (req,res)=>{
             action: 'get documents status'
         });
     }
+};
+
+const documentsChanged = (req,res)=>{
+    _student.find({
+        "$or":[{"inscriptionStatus":"En Captura"},{"inscriptionStatus":"En Proceso"}]}).then( students=>{
+            if(students.length == 0){
+               return res.status(status.NOT_FOUND).json({err:'Not found'});
+            }else{
+                const sts = students.map( stu=>({
+                    _id:stu._id,
+
+                }))
+            }
+        });
 };
 
 
