9ae6765a4d4ab6e895d2e6e03c795c6fc29817d9
kervin garcia <kegarciaca@ittepic.edu.mx>
2020-01-21 15:00:32 -0600

    Fix: Reparación de la solicitudes del ultimo dia del mes

--- controllers/reception-act/request.controller.js
+++ controllers/reception-act/request.controller.js
@@ -759,12 +759,12 @@ const groupDiary = (req, res) => {
     // StartDate.setDate(1);
     // StartDate.setMonth(data.month);
     let StartDate = new Date(data.year, data.month, 1, 0, 0, 0, 0);
-    let EndDate = new Date(StartDate.getFullYear(), data.month + 1, 0);
+    let EndDate = new Date(StartDate.getFullYear(), data.month + 1, 0,23,59,59,0);    
     let query =
         [
             {
                 $match: {
-                    "proposedDate": { $gte: StartDate, $lte: new Date(StartDate.getFullYear(), data.month + 1, 0) },
+                    "proposedDate": { $gte: StartDate, $lte: EndDate },
                     $or: [{ "phase": "Asignado", "status": "Process" }, { "phase": "Realizado" }]
                 }
             },
@@ -791,7 +791,7 @@ const groupDiary = (req, res) => {
         ];
     _request.aggregate(query, (error, diary) => {
         if (error)
-            return handler.handleError(res, status.INTERNAL_SERVER_ERROR, { error });
+            return handler.handleError(res, status.INTERNAL_SERVER_ERROR, { error });        
         _ranges.find(
             {
                 $or: [{ start: { $gte: StartDate, $lte: EndDate } },
@@ -816,7 +816,8 @@ const groupRequest = (req, res) => {
     // StartDate.setFullYear(data.year);        
     // StartDate.setMonth(data.month);
     // StartDate.setHours(0, 0, 0, 0);
-    let EndDate = new Date(StartDate.getFullYear(), data.month + 1, 0);
+    // let EndDate = new Date(StartDate.getFullYear(), data.month + 1, 0);
+    let EndDate = new Date(StartDate.getFullYear(), data.month + 1, 0,23,59,59,0);
     // console.log("Start", StartDate, "End", EndDate);
     let query =
         [
