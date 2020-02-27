8f54618470a163140d4120f960dacb1d1b8462f0
GregVaz <grvazquezya@ittepic.edu.mx>
2020-02-27 15:05:27 -0700

    Fix: Position model gender

--- models/shared/position.model.js
+++ models/shared/position.model.js
@@ -6,6 +6,10 @@ let positionSchema = mongoose.Schema({
     documents: [{type: mongoose.Schema.Types.ObjectId, ref: 'Document'}],
     canSign: {type: Boolean, default: false},
     isUnique: {type: Boolean, default: false},
+    gender: {
+        male: {type: String, uppercase: true, trim: true},
+        female: {type: String, uppercase: true, trim: true}
+    }
 });
 
 const positionModel = mongoose.model('Position', positionSchema, 'positions');
